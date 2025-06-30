c3d2c90f5f47b125991a32d8c6b4608b6039a8f0
scariola <socariola@gmail.com>
2025-06-30 22:47:51 +0800

    rework

--- src/main/java/com/facebookTimestamp/utils/TimestampUtils.java
+++ src/main/java/com/facebookTimestamp/utils/TimestampUtils.java
@@ -1,8 +1,8 @@
 package com.facebookTimestamp.utils;
 
 public class TimestampUtils {
-    public static float generateHmacTimestamp() {
-        float timeStampsss = System.currentTimeMillis() / 1000.0f;
-        return timeStampsss;
+    public static double generateHmacTimestamp() {
+        float timeStamps = System.currentTimeMillis() / 1000;
+        return timeStamps;
     }
 }
